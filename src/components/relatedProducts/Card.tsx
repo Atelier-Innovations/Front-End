71d93ce0f901e6d76c4f717aa077813ae591f9d6
Thomas Faddegon <tomfaddegon@gmail.com>
2023-01-30 16:42:26 -0500

    small card tweaks

--- src/components/relatedProducts/Card.tsx
+++ src/components/relatedProducts/Card.tsx
@@ -38,6 +38,7 @@ const Card: FC<CardProps> = (props) => {
       'https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg';
   }
 
+  // retreive data
   useEffect(() => {
     getProductDataFromDB();
     getProductImgFromDB();
