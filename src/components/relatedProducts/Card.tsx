5f5da2f634d70c345916b8e0a692e77672ab6274
Thomas Faddegon <tomfaddegon@gmail.com>
2023-01-30 15:15:15 -0500

    added function and express route to get thumbnail images from the DB and display on the card

--- src/components/relatedProducts/Card.tsx
+++ src/components/relatedProducts/Card.tsx
@@ -7,9 +7,9 @@ import axios from 'axios';
 interface CardProps {
   id: string;
 }
-
 const Card: FC<CardProps> = (props) => {
   const [productData, setProductData] = useState({});
+  const [productImage, setProductImage] = useState('');
 
   console.log('product data: ', productData);
 
@@ -25,8 +25,17 @@ const Card: FC<CardProps> = (props) => {
       });
   };
 
+  // get product img
+  const getProductImgFromDB = async () => {
+    let response = await axios.get(
+      `http://localhost:6969/products/${props.id}/styles`
+    );
+    setProductImage(response.data.results[0].photos[0].thumbnail_url);
+  };
+
   useEffect(() => {
     getProductDataFromDB();
+    getProductImgFromDB();
   }, []);
 
   // create product object type
@@ -36,20 +45,24 @@ const Card: FC<CardProps> = (props) => {
     default_price?: string;
   };
 
+  //perform type checking
   const product: ProductObject = productData;
+  const img: string = productImage;
 
   const name = product.name;
   const category = product.category;
   const price = product.default_price;
 
-  console.log('type checked product object: ', product);
-
   return (
-    <div>
-      <p>{name}</p>
-      <p>{category}</p>
-      <p>{price}</p>
-      <p>Rating</p>
+    <div className='card'>
+      <div className='cardImage' style={{ backgroundImage: `url(${img})` }}>
+        jfkldafjdlak
+      </div>
+      <div className='cardInfo'>
+        <p>{name}</p>
+        <p>{category}</p>
+        <p>{price}</p>
+      </div>
     </div>
   );
 };
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,55 +1,66 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 const express = require('express');
 const dotenv = require('dotenv').config();
 import axios from 'axios';
-var cors = require('cors')
+var cors = require('cors');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.get('/products', (req: Request, res: Response) => {
   axios('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products', {
     headers: {
-      Authorization: process.env.API_KEY
-    }
-  })
-  .then(results => {
-    res.send(results.data)
-  })
-})
+      Authorization: process.env.API_KEY,
+    },
+  }).then((results) => {
+    res.send(results.data);
+  });
+});
 
 // get product info
 app.get('/products/:id', (req: Request, res: Response) => {
-  const id = req.params.id
-  console.log('this is the product info route')
-  console.log('id: ', id)
+  const id = req.params.id;
+  console.log('this is the product info route');
+  console.log('id: ', id);
   axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${id}`, {
     headers: {
-      Authorization: process.env.API_KEY
-    }
-  })
-  .then(results => {
-    console.log('results: ', results.data)
-    res.send(results.data)
-  })
-})
-
+      Authorization: process.env.API_KEY,
+    },
+  }).then((results) => {
+    console.log('results: ', results.data);
+    res.send(results.data);
+  });
+});
 
 // get related product id's
 app.get('/products/:id/related', (req: Request, res: Response) => {
-  const id = req.params.id
-  console.log('this is the related products route')
-  console.log('id: ', id)
-  axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${id}/related`, {
-    headers: {
-      Authorization: process.env.API_KEY
+  const id = req.params.id;
+  axios(
+    `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${id}/related`,
+    {
+      headers: {
+        Authorization: process.env.API_KEY,
+      },
     }
-  })
-  .then(results => {
-    console.log('related products: ', results.data)
-    res.send(results.data)
-  })
-})
+  ).then((results) => {
+    res.send(results.data);
+  });
+});
+
+// get product styles
+app.get('/products/:id/styles', (req: Request, res: Response) => {
+  const id = req.params.id;
+  axios(
+    `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${id}/styles`,
+    {
+      headers: {
+        Authorization: process.env.API_KEY,
+      },
+    }
+  ).then((results) => {
+    res.send(results.data);
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
