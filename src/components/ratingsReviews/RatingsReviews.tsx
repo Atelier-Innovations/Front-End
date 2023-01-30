265bb0b8ccf94995a4c2fe4e75993e2d06efd702
Bryce Anderson <abryce.anderson@gmail.com>
2023-01-30 11:49:47 -0500

    changes to css and div structure based on css learning

--- src/components/ratingsReviews/RatingsReviews.tsx
+++ src/components/ratingsReviews/RatingsReviews.tsx
@@ -10,22 +10,29 @@ const RatingsReviews: FC<RatingsReviewsProps> = (props) => {
 
   return (
     <div className="widget reviews-ratings">
-      <h6>RATINGS & REVIEWS</h6>
-      <div className="ratings-panel">
-        Ratings Panel here
+      <div className="banner">
+        RATINGS & REVIEWS
       </div>
 
-      <div className="review-list">
-        Individual review components rendered here<br/>
+      <div className="overall">
+        <div className="ratings-panel">
+          Ratings Panel here
+        </div>
+
+        <div className="review-list">
+          Individual review components rendered here<br/>
 
 
 
       </div><br/>
+      </div>
 
       <div className="button-panel">
         <button>More Reviews</button>
         <button>Add A Review +</button>
       </div>
+
+
 
     </div>
   )
