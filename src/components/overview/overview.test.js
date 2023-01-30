dc70a42b5db82d320db0f9400cbc24cf25ee0727
deirdre <deirdre@hackReactor-MONGO-EDITION.myguest.virtualbox.org>
2023-01-30 15:55:33 -0500

    added fixtures.js, stubbed out preliminary tests for overview widget

--- src/components/overview/overview.test.js
+++ src/components/overview/overview.test.js
@@ -1,40 +1,154 @@
 import React from 'react';
 import Overview from './Overview';
 import StaticInfoDisplay from './controlPanel/StaticInfoDisplay';
+import StyleSelector from './controlPanel/StyleSelector';
 import renderer from 'react-test-renderer';
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom'
 import App from '../App';
-
-const camoOnesie = {
-  "id": 37311,
-  "campus": "hr-rfe",
-  "name": "Camo Onesie",
-  "slogan": "Blend in to your crowd",
-  "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
-  "category": "Jackets",
-  "default_price": "140.00",
-  "created_at": "2021-08-13T14:37:33.145Z",
-  "updated_at": "2021-08-13T14:37:33.145Z",
-  "features": [
-      {
-          "feature": "Fabric",
-          "value": "Canvas"
-      },
-      {
-          "feature": "Buttons",
-          "value": "Brass"
-      }
-  ]
-}
+import Fixtures from './fixtures.js';
 
 
-test('On mount StaticInfoDisplay should display info for Camo Onesie', () => {
-  render (
-    <StaticInfoDisplay product={camoOnesie} />
-    )
+//Product Information Tests
+describe('StaticInfoDisplay should display info for currently selected product and style', () => {
 
-    expect(screen.getByRole('heading', {level: 3})).toHaveTextContent('Jackets');
-    expect(screen.getByRole('heading', {level: 2})).toHaveTextContent('Camo Onesie');
-    expect(screen.getByText('$140.00')).toBeInTheDocument();
+  beforeEach(() => {
+    render (
+      <StaticInfoDisplay product={Fixtures.camoOnesie} style={Fixtures.oceanBlue} />
+      )
+  })
+
+  test('Item category and name should be derived from product object', () => {
+
+
+      expect(screen.getByRole('heading', {level: 3})).toHaveTextContent('Jackets');
+      expect(screen.getByRole('heading', {level: 2})).toHaveTextContent('Camo Onesie');
+  });
+
+  test('Should display sale price if not null', () => {
+    expect(screen.getByText('$100.00')).toBeInTheDocument();
+  });
+
+});
+
+describe('StyleSelector should display available styles and selected style', () => {
+    beforeEach( () => {
+      render (
+        <StyleSelector styles={Fixtures.camoOnesieStyles}/>
+      )
+    });
+
+    test('Should display the proper number of styles for a product', () => {
+      expect(screen.getAllByRole('img')).toHaveLength(6);
+    });
+
+    test('Should display the currently selected style', () => {
+      //Fill in once we decide how to indicate currently selected style.
+      //Current best guess: check for a 'selected' css class
+    })
+})
+
+describe('ButtonPanel should allow the user to select size, quantity, and add to cart', () => {
+
+  test('By default, size selector should display "Select Size"', () => {
+    //TODO
+  })
+
+  test('Only sizes that are available should appear in the size selector', () => {
+    //TODO
+  });
+
+  test('Dropdown should read "OUT OF STOCK" and be deactivated if no stock left in current style', () => {
+    //TODO
+  });
+
+
+  test('By default, quantity dropdown should display "-" and be disabled', () => {
+    //TODO
+  })
+
+  test('Once size is selected, quantity dropdown should display "1"', () => {
+    //TODO
+  })
+
+  test('Quantity dropdown should display integers ranging from 1 to maximum stock, capped at 15', () => {
+    //TODO
+  });
+
+
+  test('Add to cart button should prompt user to select size if no size selected', () => {
+    //TODO
+  });
+
+  test('Add to cart button should not appear if item is out of stock', () => {
+    //TODO
+  });
+
+  test('Add to cart button should add items to cart if size is selected', () => {
+    //Hold off on implementing this one until we know we're going to have a cart
+  })
+
+});
+
+describe('Image gallery should display images of product' () => {
+
+  test('Images displayed should correspond to currently selected style', () => {
+    //TODO
+  })
+
+  test('Choosing a new style will update images to correspond to new style', () => {
+    //TODO
+  })
+
+  test('Carousel controls will allow customers to browse between photos', () => {
+    //TODO
+  })
+
+  test('Image gallery controls will allow customers to zoom in on photos', () => {
+    //TODO
+  })
+
+  test('Image gallery will expand view when expand view button is pressed', () => {
+    //TODO
+  })
+
+  test('By default the first image will be displayed', () => {
+    //TODO
+  })
+
+  test('The image displayed will correspond to the highlighted thumbnail', () => {
+    //TODO
+  })
+
+  test('the index of the currently selected image will persist when the gallery changes style', () => {
+    //TODO
+  })
+
+  test('clicking a thumbnail should update the selected image to match', () => {
+    //TODO
+  })
+
+  test('up to 7 thumbnails will be displayed at a time in thumbnail list', () => {
+    //TODO
+  })
+
+  test('Customers should be able to change image by clicking on the arrows to either side of gallery', () => {
+    //TODO
+  })
+
+  test('Clicking on the image gallery should change it to expanded view', () => {
+    //TODO
+  })
+
+  test('clicking on the image in expanded view should zoom the image by 2.5x', () => {
+    //TODO
+  })
+
+  test('moving the mouse around in zoomed view should move the zoomed view', () => {
+    //TODO -- IDK how to test this.
+  })
+
+  test("clicking on the zoomed view should return the view to regular expanded view", () => {
+    //TODO
+  })
 })
