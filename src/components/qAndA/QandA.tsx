6319d9d6148f450a3c71cb443e30518d5095692a
Daniel <daniel.b.kwak@gmail.com>
2023-01-30 11:27:44 -0600

    QandA basic html/div structure complete

--- src/components/qAndA/QandA.tsx
+++ src/components/qAndA/QandA.tsx
@@ -11,31 +11,51 @@ const QandA: FC<QandAProps> = (props) => {
     <section className="widget q-and-a">
       <div className="q-and-a-container">
         <h1>Question and Answers</h1>
-        <div className="q-and-a-question">Question 1 text
-          <p>Q: Who what which when wehre why whether how?</p>
-          <button>Helpful? </button>
-          <div>|</div>
-          <button>add-answer-button</button>
+        <div className="search-bar">
+        <input name="search-bar-text" type="text" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."/>
+        <button className="search-button">Search Button</button>
         </div>
-        <div className="q-and-a-answer">Answer 1
-          <p>A: Answer Text in Here</p>
-          <div className="q-and-a-user"> Username Here</div>
-          <div className="q-and-a-date">Date Here May 1, 2023 </div>
-          <button>Helpful Button</button>
-          <button>Report</button>
-          <div className="q-and-a-answer-photos">
-            <p>Yes, as you can see in these photos.</p>
-            <div className="q-and-a-answer-img-container">
-              <img className="q-and-a-answer-img" />
-              <img className="q-and-a-answer-img" />
-              <img className="q-and-a-answer-img" />
-              <p> by User1337 - Seller, May1, 2019</p>
+
+        <div className="question-container">
+          <div>Q: Who what which when wehre why whether how?</div>
+          <div className="helpful-answer-btn">
+            <div className="q-btns"></div>
+            <button>Helpful? </button>
+            <button>Add-Answer-button</button>
+          </div>
+        </div>
+
+        <div className="answer-container">Answer 1
+          <p>A: Answer Text Paragraph is in Here Icing macaroon bear clar jelly beans chocolatecake. Cookie oat cake chocolate halvah jelly cake cotton candy souffle topping. Jujubes topping cake gummies lemon drops.</p>
+          <div className="answer-info">
+            <div className="user"> by Username Here</div>
+            <div className="date">Date Here May 1, 2023 </div>
+            <button className="helpful-btn">Helpful Button</button>
+            <button className="report-btn">Report</button>
+          </div>
+          <div className="answer-photos">
+            <p> Answer 2 Yes, as you can see in these photos.</p>
+            <div className="img-container">
+              <img className="q-and-a-answer-img" alt="img1"/>
+              <img className="q-and-a-answer-img" alt="img2"/>
+              <img className="q-and-a-answer-img" alt="img3"/>
+          </div>
+          <div className="answer-img-info">
+            <span> by User1337 - Seller, May1, 2019</span>
+            <div className="helpful-report-btn-container">
+              <button className="helpful-btn">Helpful? Yes '9'</button>
+              <button className="report-btn">Report</button>
             </div>
           </div>
-          <button>LOAD MORE ANSWERS</button>
+          <button className="action-btn-answers">LOAD MORE ANSWERS</button>
+          </div>
         </div>
-        <button>More Answered Questions</button>
-        <button>Add a Question +</button>
+
+        {/* Action buttons */}
+        <div className="action-btn">
+          <button className="action-btn-more-answers">More Answered Questions</button>
+          <button className="action-btn-add-question">Add a Question +</button>
+        </div>
       </div>
     </section>
   )
