dab31b8c8d8293350cb22b48a4686d1bdaed9e67
Thomas Faddegon <tomfaddegon@gmail.com>
2023-01-30 12:35:11 -0500

    created function in RelatedProducts that gets related ids from database and stores them  in state, also added a couple of express routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import { Overview } from './overview/Overview';
 import RatingsReviews from './ratingsReviews/RatingsReviews';
 import RelatedProducts from './relatedProducts/RelatedProducts';
 import QandA from './qAndA/QandA';
+import ReactModal from 'react-modal';
 
-export const App: React.FC = () => (
-  <div className="app">
-    <nav>
-    </nav>
-    <Overview />
-    <RelatedProducts />
-    <RatingsReviews />
-    <QandA />
-  </div>
+export const App: React.FC = () => {
 
-);
+  return (
+    <div className="app">
+      <nav>
+      </nav>
+      <Overview />
+      <RelatedProducts />
+      <RatingsReviews />
+      <QandA />
+    </div>
+  )
+
+};
--- src/components/relatedProducts/RelatedProducts.tsx
+++ src/components/relatedProducts/RelatedProducts.tsx
@@ -1,13 +1,39 @@
 import React, { FC } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 interface RelatedProductsProps {
 }
 
 const RelatedProducts: FC<RelatedProductsProps> = (props) => {
 
+  console.log('building rp')
+
+  //temporary currentProductId, change to props once added
+  const [currentProductId, setCurrentProductId] = useState<string>('37311')
+
+  // get related product ids from database
+  const [relatedProductIds, setRelatedProductIds] = useState<object>([])
+
+  const getRelatedProductsFromDB = () => {
+    axios.get(`http://localhost:6969/products/${currentProductId}/related`, {})
+    .then(result => {
+      setRelatedProductIds(result.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    getRelatedProductsFromDB()
+  }, [])
+
+  console.log('final related product Ids: ', relatedProductIds)
+
   return (
     <section className="related-products widget">
-      <h2 className="title">Related </h2>
+      <h2 className="title">Related Products</h2>
       <div className="carousel">
         <div className="card"></div>
         <div className="card"></div>
