637a1cc8a4184a9f6750d5209dc0ca415006ca92
deirdre <deirdre@hackReactor-MONGO-EDITION.myguest.virtualbox.org>
2023-01-31 17:03:25 -0500

    size selection now results in quantity range displayed in quantity selector NOTE: now picking a size will randomly blow the whole thing up. FIX THIS.

--- src/components/overview/controlPanel/ButtonPanel.tsx
+++ src/components/overview/controlPanel/ButtonPanel.tsx
@@ -2,26 +2,32 @@ import React from 'react';
 
 const ButtonPanel: React.FC = (props) => {
   const [outOfStock, setOutOfStock] = React.useState(false);
+  const [sizeSelected, setSizeSelected] = React.useState(0);
+  const [quantityRange, setQuantityRange] = React.useState(0);
+
+  const handleSizeChange = event => {
+    setSizeSelected(event.target.selectedIndex);
+  }
 
   React.useEffect( () => {
     if (props.skus.length > 0) {
       let total = props.skus.reduce( (mem, sku) => {
-        console.log('quantity:', props.currentStyle.skus[sku].quantity)
         return mem + props.currentStyle.skus[sku].quantity
       }, 0)
-      console.log('total:', total)
       if (!(total > 0)) {
         setOutOfStock(true);
       } else {
         setOutOfStock(false);
       }
+
+      setQuantityRange(props.currentStyle.skus[props.skus[sizeSelected]].quantity);
     }
-  }, [props.currentStyle])
+  }, [props.currentStyle, sizeSelected])
 
   return (
     <div className="controls">
       <div className="row-1">
-        <select className="size-button" disabled={outOfStock}>
+        <select className="size-button" disabled={outOfStock} onChange={handleSizeChange}>
           {outOfStock ?
           <option>OUT OF STOCK</option> : <option>Select Size</option>}
           {props.skus.map((sku, index) => {
@@ -32,7 +38,14 @@ const ButtonPanel: React.FC = (props) => {
             }
           })}
         </select>
-        <select className="quantity"></select>
+        <select className="quantity" disabled={sizeSelected === 0 ?
+                                     true : false}>
+          <option>-</option>
+          {Array.from(Array(quantityRange).keys()).map( num => (
+            <option>{num}</option>
+          ))}
+
+        </select>
       </div>
       <div className="row-2">
         <button className="add-to-bag">Add To Bag</button>
