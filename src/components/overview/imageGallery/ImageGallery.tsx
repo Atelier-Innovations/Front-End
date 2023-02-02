26cd848ed0bf77388f60fad3d5ad09823361d767
deirdre <deirdre@hackReactor-MONGO-EDITION.myguest.virtualbox.org>
2023-02-02 12:03:23 -0500

    image gallery now displaying dynamic data

--- src/components/overview/imageGallery/ImageGallery.tsx
+++ src/components/overview/imageGallery/ImageGallery.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import ImageCarousel from './ImageCarousel';
+const expand = require('../../../icons/expand-solid.svg');
 
-const ImageGallery: React.FC = (props) => {
+interface ImageGalleryProps {
+  style: object
+}
+
+const ImageGallery: React.FC<ImageGalleryProps> = (props: ImageGalleryProps) => {
+  const [currentImage, setCurrentImage] = React.useState('');
+
+  React.useEffect( () => {
+    if (props.style.photos) {
+      setCurrentImage(props.style.photos[0].url)
+    }
+  }, [props.style])
+
   return (
     <div className="image-gallery">
+      <img className="expand-icon" src={expand} />
+      <img src={currentImage} />
       <ImageCarousel />
     </div>
   );
